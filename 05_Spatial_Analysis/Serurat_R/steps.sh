64cf5777b8fc174684c8833ba31fd2b5485022d9
wsl-gao <bioinfo.gao@gmail.com>
2026-02-28 19:14:31 -0500

    aa2

--- 05_Spatial_Analysis/Serurat_R/steps.sh
+++ 05_Spatial_Analysis/Serurat_R/steps.sh
@@ -7,3 +7,11 @@ mamba create -n spatial_R \
 
 mamba activate spatial_R    
 R --version
+
+https://posit.co/download/rstudio-server/
+  174  sudo apt-get install gdebi-core
+  175  wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2026.01.1-403-amd64.deb
+  176  sudo gdebi rstudio-server-2026.01.1-403-amd64.deb
+
+sudo vim /etc/rstudio/rserver.conf
+rsession-which-r=/home/zhen/miniforge3/envs/spatial_R/bin/R
