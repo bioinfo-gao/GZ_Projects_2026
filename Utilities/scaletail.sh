453b2a11226829643e0060040f869e43a4b3257b
ZG_Tailscale <bioinfo.gao@gmail.com>
2026-03-21 21:47:45 -0400

    26

---
+++ Utilities/scaletail.sh
@@ -0,0 +1,27 @@
+# 1) install scaltale:
+
+curl -fsSL https://tailscale.com/install.sh | sh
+# Installing Tailscale for ubuntu noble, using method apt
+# + sudo mkdir -p --mode=0755 /usr/share/keyrings
+
+# 2) then follow the link to login 
+https://login.tailscale.com/uinv/idhjz8zmZ221hTrUu4d8K11 
+
+# 3) In local linux computer 
+scale login 
+
+# To authenticate, visit:
+# 	https://login.tailscale.com/a/1f8dbbf801daf1
+
+# needs approval 
+
+# 4) ssh
+ssh saveguest@us1.tail667c81.ts.net
+
+# 5) obtain the fake IP
+tailscale ip
+100.101.19.30
+fd7a:115c:a1e0::1501:13b0
+
+
+
