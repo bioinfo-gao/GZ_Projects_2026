1afae0fcadb9170148988fae166936deb8f2c2ad
ZG_Tailscale <bioinfo.gao@gmail.com>
2026-03-25 17:27:41 -0400

    ff8

--- Utilities/R/2026_Tested_Versions/Ch01/01_07/Ex01_07.R
+++ Utilities/R/2026_Tested_Versions/Ch01/01_07/Ex01_07.R
@@ -12,15 +12,30 @@ browseURL("http://cran.r-project.org/doc/manuals/R-data.html#Reading-Excel-sprea
 # This command gives an error on missing data
 # but works on complete data
 # "header = TRUE" means the first line is a header
-trends.txt <- read.table("~/Desktop/R/GoogleTrends.txt", header = TRUE)
+# trends.txt <- read.table("~/Desktop/R/GoogleTrends.txt", header = TRUE)
+trends.txt <- read.table("/home/gao/projects/Utilities/R/2026_Tested_Versions/Ch01/01_07/GoogleTrends.csv", sep = ",", header = TRUE)
 ?read.table
 
 # This works with missing data by specifying the
 # separator: \t is for tabs, sep = "," for commas
 # R converts missing to "NA"
-trends.txt <- read.table("~/Desktop/R/GoogleTrends.txt", header = TRUE, sep = "\t")
+# trends.txt <- read.table("~/Desktop/R/GoogleTrends.txt", header = TRUE, sep = "\t")
 str(trends.txt)  # This gives structure of object sntxt
 # Or click on file in Workspace viewer, which brings up this:
+
+
+# Example code snippet
+# install.packages("gtrendsR") # quotation marks are important, cannot be omitted !!
+# library(gtrendsR)
+# res <- gtrends(c("Hand sanitizer"), geo = "US", time = "now 1-H")
+# res$interest_over_time
+# getwd()
+# setwd("/home/gao/projects/Utilities/R/2026_Tested_Versions")
+# write.csv(res$interest_over_time, "trends.csv")
+
+# trends.txt <- read.table("/home/gao/projects/Utilities/R/2026_Tested_Versions/trends.csv", header = TRUE)
+
+
 View(trends.txt)
 ?View
 
